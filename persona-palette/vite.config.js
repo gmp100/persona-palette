b42241c035d4625ab94106852f8af27c4e76f6df
gaurav <gp200patil@gmail.com>
2025-09-08 13:55:01 +0530

    Deploy ready build

--- persona-palette/vite.config.js
+++ persona-palette/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: '/persona-palette/',   // repo ka exact naam
+  plugins: [react(),tailwindcss()],
 })
